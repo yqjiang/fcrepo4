afcd445eeeaa5f4df962f9241555796d11fd0eb5
ajs6f <ajs6f@virginia.edu>
2013-05-21 12:45:08 -0400

    Got 401 returning from protected resource. Now just have to find out how to log in...

--- fcrepo-http-commons/src/main/java/org/fcrepo/responses/HtmlProvider.java
+++ fcrepo-http-commons/src/main/java/org/fcrepo/responses/HtmlProvider.java
@@ -32,7 +32,6 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
-import com.hp.hpl.jena.graph.Node;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
@@ -44,6 +43,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.google.common.collect.ImmutableMap.Builder;
+import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.query.Dataset;
 
 /**
